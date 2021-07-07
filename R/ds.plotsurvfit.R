5a368da732e0918b9473df954288cdaf25bfc2d6
Soumya Banerjee <neel.soumya@gmail.com>
2021-07-07 11:32:17 +0100

    Update ds.plotsurvfit.R

--- R/ds.plotsurvfit.R
+++ R/ds.plotsurvfit.R
@@ -12,6 +12,13 @@
 #' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login. 
 #' If the \code{datasources} argument is not specified
 #' the default set of connections will be used: see \code{\link{datashield.connections_default}}.
+#' @param method_anonymization an integer. Method of anonymization to be used (1: deterministic, 2: probabilistic). Default value is 2.
+#' @param noise an integer. fraction of noise (between 0 and 1) to be added to original data. 
+#'     Noise is added as a percentage of original value. 
+#'     This is used for probabilistic anonymization.
+#'     Default value is 0.03 
+#' @param knn an integer. Number of nearest neighbours to be used for k nearest neighbours algoithm (for determinstic anonymization). 
+#'     Default value is 20.
 #' @return privacy preserving survival curve from the server side environment.
 #' @author Soumya Banerjee, Demetris Avraam, Paul Burton, Xavier Escriba-Montagut, Juan Gonzalez and Tom Bishop (2021).
 #' @examples
@@ -69,11 +76,12 @@
 ds.plotsurvfit <- function(formula = NULL,
                            dataName = NULL,
                            fun = NULL,
-                           datasources = NULL)
+                           datasources = NULL,
+                           method_anonymization = 2,
+                           noise = 0.03,
+                           knn = 20
+			  )
 {
-  # method_anonymization = 2,
-	#		noise = 0.03,
-	#		knn = 20
   
   # look for DS connections
   # if one not provided then get current
@@ -95,7 +103,8 @@ ds.plotsurvfit <- function(formula = NULL,
   }
   
   
-  calltext <- call("plotsurvfitDS", formula=formula, dataName) #, method_anonymization, noise, knn)
+  calltext <- call("plotsurvfitDS", formula=formula, dataName, method_anonymization, noise, knn)
+  # calltext <- call("plotsurvfitDS", formula=formula, dataName) #, method_anonymization, noise, knn)	
   
   # call aggregate function
   output <- datashield.aggregate(datasources, calltext)
